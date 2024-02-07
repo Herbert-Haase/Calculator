5425cbabf80c82c7883089dc0f590b6c801c58a0
Herbert-Haase <herberthaase02@gmx.de>
2024-02-07 21:07:01 +0100

    map buttons to functions

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-function add(num1, num2) {
-  return num1 + num2;
+let operators = {
+  "+" : (num1, num2) => num1 + num2,
+  "-" : (num1, num2) => num1 - num2,
+  "/" : (num1, num2) => num1 / num2,
+  "*" : (num1, num2) => num1 * num2,
 }
 
-function subtract(num1, num2) {
-  return num1 - num2;
+let num1 = ""
+let num2 = ""
+let operator = null
+
+
+function operate(operator, num1, num2) {
+  return operator(+num1, +num2)
 }
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (button.className == "number") {
+      if (operator !== null) {
+        num1 += button.textContent
+      }
+      else {
+        num2 += button.textContent
+      }
+    }
+    else if (button.className == "operator") {
+      if (operator === null) {
+        operator = operators[button.textContent]
+      }      
+      else if (num1 !== "" || num2 !== "") {
+        num1 = operate(operator, num1, num2)
+        num2 = ""
+        operator = operators[button.textContent]
+      }
+    } 
 
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-let num1;
-let num2;
-let operator;
+    })
+  })
