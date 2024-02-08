7dc4984a8b2eb24764460a01e969a68c78bdf49c
Herbert-Haase <herberthaase02@gmx.de>
2024-02-08 17:36:48 +0100

    add display function

--- main.js
+++ main.js
@@ -8,16 +8,23 @@ let operators = {
 let num1 = ""
 let num2 = ""
 let operator = null
+let operatorString = ""
 
 
 function operate(operator, num1, num2) {
   return operator(+num1, +num2)
 }
 
+function display(operator, operatorString, num1, num2) {
+  displayString = `${num1} ${operatorString} ${num2}`
+  displaySpace = document.querySelector("#display")
+  displaySpace.textContent = displayString
+}
+
 document.querySelectorAll("button").forEach((button) => {
   button.addEventListener("click", (e) => {
     if (button.className == "number") {
-      if (operator !== null) {
+      if (operator === null) {
         num1 += button.textContent
       }
       else {
@@ -27,13 +34,15 @@ document.querySelectorAll("button").forEach((button) => {
     else if (button.className == "operator") {
       if (operator === null) {
         operator = operators[button.textContent]
+        operatorString = button.textContent
       }      
       else if (num1 !== "" || num2 !== "") {
         num1 = operate(operator, num1, num2)
         num2 = ""
         operator = operators[button.textContent]
+        operatorString = button.textContent
       }
     } 
-
+    display(operator, operatorString, num1, num2)
     })
   })
