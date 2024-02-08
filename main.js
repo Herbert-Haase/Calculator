f9181e191767dfa0c743fb2310587cde843a97de
Herbert-Haase <herberthaase02@gmx.de>
2024-02-08 20:53:09 +0100

    prevent divide by zero, add clear, add equals, add decimal

--- main.js
+++ main.js
@@ -18,9 +18,54 @@ function operate(operator, num1, num2) {
 function display(operator, operatorString, num1, num2) {
   displayString = `${num1} ${operatorString} ${num2}`
   displaySpace = document.querySelector("#display")
+
+  // divide by Zero
+  if (num2 === "0" && operatorString == "/") {displayString = "Error Divide by Zero"}
   displaySpace.textContent = displayString
 }
 
+// clear button 
+function clear() {
+  operator = null
+  operatorString = ""
+  num1 = ""
+  num2 = ""
+}
+
+document.querySelector(".clear").addEventListener("click", () => {
+  clear()
+  display(operator, operatorString, num1, num2)
+})
+
+// equals button
+function equals() {
+        num1 = operate(operator, num1, num2)
+        num2 = ""
+}
+
+document.querySelector(".equals").addEventListener("click", () => {
+  equals()
+  operatorString = ""
+  operator = null
+  display()
+})
+
+// decimal button
+document.querySelector(".decimal").addEventListener("click", () => {
+      if (operator === null) {
+        if (num1.search("\\.") == -1) {
+          num1 += "."
+        }
+      }
+      else {
+        if (num2.search("\\.") == -1) {
+          num2 += "."
+        }
+      }
+})
+
+
+// numbers and operators buttons
 document.querySelectorAll("button").forEach((button) => {
   button.addEventListener("click", (e) => {
     if (button.className == "number") {
@@ -37,10 +82,9 @@ document.querySelectorAll("button").forEach((button) => {
         operatorString = button.textContent
       }      
       else if (num1 !== "" || num2 !== "") {
-        num1 = operate(operator, num1, num2)
-        num2 = ""
-        operator = operators[button.textContent]
+        equals()
         operatorString = button.textContent
+        operator = operators[button.textContent]
       }
     } 
     display(operator, operatorString, num1, num2)
