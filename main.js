44b74ae1b194a33199928506025d573cd490b2d5
Herbert-Haase <herberthaase02@gmx.de>
2024-02-09 16:19:25 +0100

    refactor functions

--- main.js
+++ main.js
@@ -1,92 +1,81 @@
-let operators = {
-  "+" : (num1, num2) => num1 + num2,
-  "-" : (num1, num2) => num1 - num2,
-  "/" : (num1, num2) => num1 / num2,
-  "*" : (num1, num2) => num1 * num2,
-}
+const operators = {
+  "+": (num1, num2) => num1 + num2,
+  "-": (num1, num2) => num1 - num2,
+  "/": (num1, num2) => num1 / num2,
+  "*": (num1, num2) => num1 * num2,
+};
 
-let num1 = ""
-let num2 = ""
-let operator = null
-let operatorString = ""
-
+let num1 = "";
+let num2 = "";
+let operator = null;
+let operatorString = "";
+const displaySpace = document.querySelector("#display");
 
 function operate(operator, num1, num2) {
-  return operator(+num1, +num2)
+  return operator(+num1, +num2);
 }
 
-function display(operator, operatorString, num1, num2) {
-  displayString = `${num1} ${operatorString} ${num2}`
-  displaySpace = document.querySelector("#display")
+function display() {
+  const displayString = `${num1} ${operatorString} ${num2}`;
 
-  // divide by Zero
-  if (num2 === "0" && operatorString == "/") {displayString = "Error Divide by Zero"}
-  displaySpace.textContent = displayString
+  // Divide by Zero
+  if (num2 === "0" && operatorString === "/") {
+    displaySpace.textContent = "Error: Divide by Zero";
+  } else {
+    displaySpace.textContent = displayString;
+  }
 }
 
-// clear button 
+// Clear button
 function clear() {
-  operator = null
-  operatorString = ""
-  num1 = ""
-  num2 = ""
+  operator = null;
+  operatorString = "";
+  num1 = "";
+  num2 = "";
 }
 
 document.querySelector(".clear").addEventListener("click", () => {
-  clear()
-  display(operator, operatorString, num1, num2)
-})
+  clear();
+  display();
+});
 
-// equals button
+// Equals button
 function equals() {
-        num1 = operate(operator, num1, num2)
-        num2 = ""
+  num1 = operate(operator, num1, num2);
+  num2 = "";
 }
 
 document.querySelector(".equals").addEventListener("click", () => {
-  equals()
-  operatorString = ""
-  operator = null
-  display()
-})
+  equals();
+  operatorString = "";
+  operator = null;
+  display();
+});
 
-// decimal button
+// Decimal button
 document.querySelector(".decimal").addEventListener("click", () => {
-      if (operator === null) {
-        if (num1.search("\\.") == -1) {
-          num1 += "."
-        }
-      }
-      else {
-        if (num2.search("\\.") == -1) {
-          num2 += "."
-        }
-      }
-})
+  let currentNum = operator === null ? num1 : num2;
+  if (!currentNum.includes(".")) {
+    currentNum += ".";
+    operator === null ? (num1 = currentNum) : (num2 = currentNum);
+  }
+});
 
-
-// numbers and operators buttons
+// Numbers and operators buttons
 document.querySelectorAll("button").forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (button.className == "number") {
+    if (button.className === "number") {
+      operator === null ? (num1 += button.textContent) : (num2 += button.textContent);
+    } else if (button.className === "operator") {
       if (operator === null) {
-        num1 += button.textContent
-      }
-      else {
-        num2 += button.textContent
+        operator = operators[button.textContent];
+        operatorString = button.textContent;
+      } else if (num1 !== "" || num2 !== "") {
+        equals();
+        operatorString = button.textContent;
+        operator = operators[button.textContent];
       }
     }
-    else if (button.className == "operator") {
-      if (operator === null) {
-        operator = operators[button.textContent]
-        operatorString = button.textContent
-      }      
-      else if (num1 !== "" || num2 !== "") {
-        equals()
-        operatorString = button.textContent
-        operator = operators[button.textContent]
-      }
-    } 
-    display(operator, operatorString, num1, num2)
-    })
-  })
+    display();
+  });
+});
